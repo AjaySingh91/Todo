567a51f702bf2cdf36121ba6216328f12eac37c5
Ajay <ajaykumar.18158@marwadieducation.edu.in>
2023-06-10 16:17:25 +0530

    Todo App

---
+++ src/main.tsx
@@ -0,0 +1,33 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+
+const queryClient = new QueryClient()
+
+// {
+//   defaultOptions:{
+//     queries:{
+//       retry: 3,
+//       cacheTime: 300000,
+//       staleTime: 10*1000,                       defaul setting for the queryclient
+//       refetchOnWindowFocus: false,
+//       refetchOnReconnect: false,
+//       refetchOnMount: false
+//     }
+//   }
+// }
+ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+    <App />
+    <ReactQueryDevtools />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
---
+++ src/react-query/TodoForm.tsx
@@ -0,0 +1,75 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+import { useRef } from 'react';
+import { Todo } from './hooks/useTodos';
+import APIClient from './services/apiClient';
+import { v4 as uuidv4 } from 'uuid';
+
+
+const apiClient = new APIClient<Todo>('/todos');
+
+interface AddTodoContext {
+  previousTodos: Todo[]
+}
+
+const TodoForm = () => {
+  const queryClient = useQueryClient();
+
+  const addTodo = useMutation<Todo, Error,Todo, AddTodoContext>({
+    mutationFn: async (newTodo) => {
+      const todoWithId = { ...newTodo, id: uuidv4() }
+      const response = await apiClient.post<Todo>(newTodo);
+      return response.data;
+    },
+
+  onMutate:(newTodo: Todo) => {
+    const previousTodos = queryClient.getQueryData<Todo[]>(['todos']) || [];
+    queryClient.setQueriesData<Todo[]>(['todos'], todos => [newTodo, ...(todos || [])])
+    if(ref.current) ref.current.value = '';
+    return {previousTodos}
+  },
+  onSuccess: (savedTodo ,newTodo) =>{
+      // Approach : invalidating the cache for fetching agin for new added data
+      // queryClient.invalidateQueries({
+      //   queryKey: ['todos']
+      // })
+
+      queryClient.setQueryData<Todo[]>(['todos'], todos => todos?.map(todo => todo === newTodo? savedTodo : todo))
+    
+    },
+    onError: (error, newTodo,context) => {
+      if (!context) return;
+
+      queryClient.setQueryData<Todo[]>(['todos'], context.previousTodos)
+    }
+  });
+  const ref = useRef<HTMLInputElement>(null);
+
+  return (
+    <>
+   { addTodo.error && <div className="alert alert-danger">{
+    addTodo.error.message
+   }</div>}
+    <form className="row mb-3" onSubmit={event => {
+      event.preventDefault();
+
+      if(ref.current && ref.current.value)
+      addTodo.mutate({
+        id:0,
+        title: ref.current?.value,
+        completed: false,
+        userId: 1,
+      });
+    }}>
+      <div className="col">
+        <input ref={ref} type="text" className="form-control" />
+      </div>
+      <div className="col">
+        <button disabled={addTodo.isLoading} className="btn btn-primary">{addTodo.isLoading? 'Adding...' : 'Add'}</button>
+      </div>
+    </form>
+    </>
+  );
+};
+
+export default TodoForm;
